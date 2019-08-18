792c43ecd9b11b46a9182fd53ba796fb25ba6109
Otegami <a.s.takuya1026@gmai.com>
2019-08-18 23:06:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { Table, Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentWillMount() {
+    axios.get('http://localhost:3000/books').then((response) => {
+      this.setState({
+        books: response.data
+      })
+    });
+  }
+
+  render(){
+    let books = this.state.books.map((book) => {
+      return(
+        <tr key={book.id}>
+          <td>{book.id}</td>
+          <td>{book.title}</td>
+          <td>{book.rating}</td>
+          <td>
+            <Button color="success" size="sm" className="mr-2">Edit</Button>
+            <Button color="danger" size="sm">Delete</Button>
+          </td>
+        </tr>
+      )
+    });
+    return (
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default App;
